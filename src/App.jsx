c6d4e3d6f0427673112d61662359ab50044533f7
RezaPahlevi1 <flashwatchreza@gmail.com>
2025-10-09 14:07:19 +0700

    feat: react & tailwind configuration

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-8xl">Reza Porto</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
